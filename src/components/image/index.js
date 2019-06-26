1189cb8997b24556bc9ac3041d389448148a7243
Andre Zimpel <andrezimpel@users.noreply.github.com>
2019-06-26 16:18:49 +0200

    Update index.js

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import TrackVisibility from 'react-on-screen';
 import styles from './index.module.scss';
 
@@ -8,7 +9,7 @@ class Image extends Component {
     if (this.props.image === undefined) { return null; }
 
     return (
-      <TrackVisibility once partialVisibility offset={600}>
+      <TrackVisibility once partialVisibility offset={30}>
         {({ isVisible }) => {
           if (isVisible) {
             return <ResponsiveImageComponent {...this.props}/>
@@ -26,10 +27,13 @@ class ResponsiveImageComponent extends Component {
 
     this.state = {
       width: this.props.defaultWidth,
-      height: null
+      height: null,
+      loaded: false,
+      fit: 'thumb'
     }
 
     this.calculateSize = this.calculateSize.bind(this);
+    this.onImageLoad = this.onImageLoad.bind(this);
   }
 
   componentDidMount() {
@@ -43,34 +47,44 @@ class ResponsiveImageComponent extends Component {
 
   calculateSize() {
     const { stateHeight, stateWidth } = this.state;
-    const height = Math.round(this.wrapper.getBoundingClientRect().height);
-    const width = Math.round(this.wrapper.getBoundingClientRect().width);
+    let height = Math.round(this.wrapper.getBoundingClientRect().height);
+    let width = Math.round(this.wrapper.getBoundingClientRect().width);
+
+    if (width > 2000) {
+      const ratio = 2000 / width;
+      width = 2000;
+      height = parseInt(height * ratio, 10);
+    }
 
     if (stateHeight !== height || stateWidth !== width) {
       this.setState({ height, width });
     }
   }
 
+  onImageLoad() {
+    this.setState({ loaded: true });
+  }
+
   render() {
     const { image, showCaption } = this.props;
     const url = image.file.url;
     const details = image.file.details.image;
-    const { height, width } = this.state;
+    const { height, width, loaded, fit } = this.state;
     const ratio = this.props.ratio ? this.props.ratio : (details.height / details.width * 100);
 
-    const imgUrl = url + '?w=' + width + '&h=' + height + '&fit=thumb';
-    const imgUrl2x = url + '?w=' + (width*2) + '&h=' + (height*2) + '&fit=thumb 2x';
-    const imgUrl3x = url + '?w=' + (width*3) + '&h=' + (height*3) + '&fit=thumb 3x';
+    const imgUrl = url + '?w=' + width + '&h=' + height + '&fit=' + fit + '&q=90';
+    const imgUrl2x = url + '?w=' + (width*2) + '&h=' + (height*2) + '&fit=' + fit + '&q=70 2x';
+    const imgUrl3x = url + '?w=' + (width*3) + '&h=' + (height*3) + '&fit=' + fit + '&q=50 3x';
 
     return (
-      <figure key={image.contentful_id} className={styles.figure}>
+      <figure key={image.contentfulId} className={styles.figure}>
         <div ref={(ref) => this.wrapper = ref} className={styles.wrapper} style={{ paddingTop: parseFloat(ratio).toFixed(2) + '%'}}>
-          {((width && height) &&
-            <img className={styles.image} src={imgUrl} srcSet={imgUrl2x + ',' + imgUrl3x} alt={image.description} title={image.title}/>
+          {((width !== null && height !== null) &&
+            <img className={styles.image} src={imgUrl} srcSet={`${imgUrl}, ${imgUrl2x}, ${imgUrl3x}`} alt={image.description} title={image.title} onLoad={this.onImageLoad} data-loaded={loaded}/>
           )}
         </div>
-        {((showCaption && image.description) &&
-          <figcaption>{image.description}</figcaption>
+        {((showCaption && image.title) &&
+          <figcaption>{image.title}</figcaption>
         )}
       </figure>
     );
@@ -85,8 +99,7 @@ Image.propTypes = {
 }
 
 Image.defaultProps = {
-  defaultWidth: 500,
-  showCaption: false
+  defaultWidth: 500
 }
 
 export default Image;
