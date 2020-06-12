b5a6ed80c34859bd1bc3548ff72b8f26c10f8e95
Andre Zimpel <andre@unknownbasics.com>
2020-06-12 06:18:05 +0200

    added site title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import Seo from '../components/seo';
 
 const Home = () => (
   <Layout>
+    <Seo
+      title={'Home'}
+    />
     <div>welcome</div>
   </Layout>
 )
 
 export default Home;
 
-// <Seo
-//   title={title}
-//   metaTitle={metaTitle}
-//   metaDescription={metaDescription && metaDescription.metaDescription}
-//   sharingTitle={sharingTitle}
-//   sharingDescription={sharingDescription && sharingDescription.sharingDescription}
-//   sharingImage={sharingImage && sharingImage.file && sharingImage.file.url}
-//   currentUrl={pathTo({ __typename: 'ContentfulPage', slug: slug })}
-//   keywords={metaKeywords}
-// />
+
+// metaTitle={metaTitle}
+// metaDescription={metaDescription && metaDescription.metaDescription}
+// sharingTitle={sharingTitle}
+// sharingDescription={sharingDescription && sharingDescription.sharingDescription}
+// sharingImage={sharingImage && sharingImage.file && sharingImage.file.url}
+// currentUrl={pathTo({ __typename: 'ContentfulPage', slug: slug })}
+// keywords={metaKeywords}
+
 // <Layout>
 //   <h1>{title}</h1>
 //   <div className={styles.sections}>
