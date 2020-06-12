ed1c9cc46e76c77e1de4a3744f097582fd57a843
Andre Zimpel <andre@unknownbasics.com>
2020-06-12 06:13:10 +0200

    updated footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,7 +5,13 @@ import styles from './index.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      footer
+      <div className={styles.copyright}>
+        &copy; Copyright {(new Date()).getFullYear()} VideoVision
+      </div>
+      <div className={styles.links}>
+        <a className={styles.link} href="https://www.videovision-chemnitz.de/impressum/" rel="noreferrer" target="_blank">Impressum</a>
+        <a className={styles.link} href="https://www.videovision-chemnitz.de/datenschutzerklaerung/" rel="noreferrer" target="_blank">Datenschutz</a>
+      </div>
     </footer>
   )
 };
