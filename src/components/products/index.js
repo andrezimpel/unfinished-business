4da24fcc8756abc7c84ef1fc4618c20cd894269d
Andre Zimpel <andre@unknownbasics.com>
2020-12-21 07:35:47 +0100

    grid

--- src/components/products/index.js
+++ src/components/products/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useStaticQuery, graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 import Container from '../container';
 import Headline from '../headline';
@@ -15,7 +16,13 @@ const Products = () => {
             id
             title
             images {
-              originalSrc
+              localFile {
+                childImageSharp {
+                  fluid(maxWidth: 400) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
             }
             shopifyId
             descriptionHtml
@@ -30,24 +37,40 @@ const Products = () => {
     }
   `);
 
-  const Product = ({ title, variants }) => {
+  const Product = ({ title, image, variants }) => {
     const currentVariant = variants[0];
     console.log('variant', currentVariant);
+    console.log('image', image);
     return (
-      <div className={styles.title}>{title}</div>
+      <div className={styles.product}>
+        <div className={styles.image}>
+          <Img fluid={image.localFile.childImageSharp.fluid}/>
+        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.price}>{currentVariant.price}</div>
+        {( variants.length > 1 &&
+          <select>
+            {( variants.map((variant, index) => (
+              <option key={index}>{variant.price} - {variant.title}</option>
+            )))}
+          </select>
+        )}
+      </div>
     )
   };
 
-  const items = products.edges.map(node => <Product key={node.node.id} title={node.node.title} variants={node.node.variants}/>);
+  const items = products.edges.map(node => <Product key={node.node.id} title={node.node.title} variants={node.node.variants} image={[...node.node.images][0]}/>);
 
   return (
     <div className={styles.products}>
-      <Headline>
-        <span>Unterstütze</span> Uns
-      </Headline>
-      <div className={styles.grid}>
-        {items}
-      </div>
+      <Container>
+        <Headline>
+          <span>Unterstütze</span> Uns
+        </Headline>
+        <div className={styles.grid}>
+          {items}
+        </div>
+      </Container>
     </div>
   )
 }
